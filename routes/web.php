b86b2e45bf4ba84fa62177b27eb197dc54ce324c
Shaheed <shaheedmhmed@gmail.com>
2025-08-12 00:07:27 +0300

    feat: add initial Vue.js app setup with routing and assets

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/{any}', function () {
+    $path = public_path('index.html');
+    if (File::exists($path)) {
+        return File::get($path);
+    }
+    abort(404);
+})->where('any', '.*');
